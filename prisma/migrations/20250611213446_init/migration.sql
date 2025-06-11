73abeaa93a1d0ea34c40122c8df40d443ed51b12
Jair_Junior83 <jaircezar83@gmail.com>
2025-06-11 18:36:55 -0300

    Criação do projeto foi adcionado o prisma foi criado as tabelas

---
+++ prisma/migrations/20250611213446_init/migration.sql
@@ -0,0 +1,61 @@
+-- CreateEnum
+CREATE TYPE "Categoria" AS ENUM ('Pizza', 'Acai');
+
+-- CreateEnum
+CREATE TYPE "StatusPedidos" AS ENUM ('Inicializado', 'Analise', 'Pago', 'Enviado', 'Entregue', 'Cancelado');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Produto" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "preco" MONEY NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "imagem" TEXT NOT NULL,
+    "categoria" "Categoria" NOT NULL,
+
+    CONSTRAINT "Produto_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Pedido" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "obs" TEXT,
+    "status" "StatusPedidos" NOT NULL DEFAULT 'Inicializado',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Pedido_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PedidoProduto" (
+    "id" SERIAL NOT NULL,
+    "produtoId" INTEGER NOT NULL,
+    "pedidoId" INTEGER NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+
+    CONSTRAINT "PedidoProduto_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Pedido" ADD CONSTRAINT "Pedido_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PedidoProduto" ADD CONSTRAINT "PedidoProduto_pedidoId_fkey" FOREIGN KEY ("pedidoId") REFERENCES "Pedido"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PedidoProduto" ADD CONSTRAINT "PedidoProduto_produtoId_fkey" FOREIGN KEY ("produtoId") REFERENCES "Produto"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
