ede7a8953a85e5b08e6d1e72da30eb8da7cd8824
Jair_Junior83 <jaircezar83@gmail.com>
2025-06-19 20:07:07 -0300

    atualização do componente dadoslist

--- src/components/home/dadosList.tsx
+++ src/components/home/dadosList.tsx
@@ -4,21 +4,21 @@ import { useProduto } from "@/store/produto";
 import { DadosItem } from "./dadosItem";
 import { useEffect } from "react";
 
-type prop = {
+type Props = {
   dados: Produto[];
 };
 
-export const DadosList = ({ dados }: prop) => {
-  const { produtos, setProdutos ,busca} = useProduto();
+export const DadosList = ({ dados }: Props) => {
+  const { produtos, setProdutos, busca } = useProduto();
 
-  useEffect(() => setProdutos(dados), [dados,setProdutos]);
+  useEffect(() => setProdutos(dados), [dados, setProdutos]);
 
-  const filtrados = produtos.filter((p) =>
-    p.name.toLowerCase().includes(busca.toLowerCase()) ||
-    p.categoria.toLowerCase().includes(busca.toLowerCase())
-  )
+  const filtrados = produtos.filter(
+    (p) =>
+      p.name.toLowerCase().includes(busca.toLowerCase()) ||
+      p.categoria.toLowerCase().includes(busca.toLowerCase())
+  );
 
-  if (!produtos) return null;
   return (
     <div className="grid grid-cols-1  sm:grid-cols-2   md:grid-cols-3 xl:grid-cols-4 gap-10">
       {filtrados.map((item) => (
